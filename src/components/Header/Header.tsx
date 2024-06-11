d6ae8cdcdf847f8087460617545118795b4617b8
Loego <atos3star@gmail.com>
2024-06-11 16:19:47 +0200

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -94,8 +94,8 @@ export const Header = () => {
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              className="h-8 w-auto rounded-lg"
+              src="https://static.ston.fi/logo/ton_symbol.png"
               alt=""
             />
           </a>
